4681716445b4bf892327cf70d846d4a6bdfc8392
songzuogang <1170031164@qq.com>
2024-06-05 21:22:59 +0800

    Initial commit

---
+++ Math/src/Add.java
@@ -0,0 +1,13 @@
+/**
+ * 功能：
+ * 作者：刚
+ * 日期：2024/6/5 21:10
+ */
+public class Add {
+    public static void main(String[] args) {
+        int a =10;
+        int b = 20;
+        int sum = a +b;
+        System.out.println(sum);
+    }
+}
